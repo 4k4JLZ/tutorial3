5684b11ce5162f32e68731496648ecc16a555ffd
1 <1970901980@qq.com>
2026-09-22 17:22:38 +0800

    Implement substract method, fix failing test

--- src/main/java/org/example/Calc.java
+++ src/main/java/org/example/Calc.java
@@ -1,14 +1,15 @@
 package org.example;
-
 public class Calc {
-
-    public int add(int x, int y)
-    {
-        return x+y;
+    public int add(int a, int b){
+        return a+b;
+    }
+    // 新增减法方法
+    public int subtract(int a, int b){
+        return a - b;
     }
 
-    public int subtract(int x, int y)
-    {
-        return x*y;
+    public static void main(String[] args) {
+        // 你的主程序代码
     }
 }
+
